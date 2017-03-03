dcdbecac5f20d5a7b6f0d2395cbefd9f26c147a3
Alexander Savinov <svnvav@mail.ru>
2017-03-03 16:52:09 +0300

    add angular and bootstrap

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/'));
 
 app.listen(3000);
